01c2875eb3b7371f4050879e396130e314a45eff
SNeXT <snext2004@gmail.com>
2024-11-10 21:19:04 +0700

    create router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import logo from '../assets/logo-nextshop.png'
+import logo from '../assets/logo-nextshop.png';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-          <img src={logo}></img>
-          NEXTSHOP
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="NextShop" />
+              NEXTSHOP
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-              <Nav.Link href="/login"><FaUser /> Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link><FaUser /> Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
