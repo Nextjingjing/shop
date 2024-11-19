428ece5e1efc3f98166a6fd67aaf8b932c4b5b0e
SNeXT <snext2004@gmail.com>
2024-11-20 03:39:50 +0700

    redux product real use

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -4,28 +4,26 @@ import {Row, Col} from 'react-bootstrap'
 // import product from '../products'
 import Product from '../components/ProductComponent'
 import axios from 'axios'
+import { useGetProductsQuery } from '../slices/apiProductsSlice';
 
 export const HomePage = () => {
-  const [product, setProduct] = useState([]);
-  useEffect(()=>{
-    const fetchProducts = async () =>{
-      const  {data}  = await axios.get('/api/products')
-      setProduct(data);
-    }; 
-  fetchProducts();
-  }
-  ,[])
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
-        <h1>Latest Product</h1>
+    {isLoading? (<div>Loading...</div>) : error ? (<div>{ error?.data?.message || error.error }</div>): (
+      <>
+      <h1>Latest Product</h1>
         <Row>
-            { product.map((product)=> (
+            { products.map((product)=> (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product}/>
                 </Col>
             ))}
         </Row>
+      </>
+    )}
+        
     </>
   )
 }
